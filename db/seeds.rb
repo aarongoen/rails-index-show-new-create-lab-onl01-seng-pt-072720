39513f07cea6edf0debba235e8d54650c3b8f7ef
aarongoen <aarongoen2.0@gmail.com>
2020-12-31 14:13:24 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-groupon = Coupon.create({coupon_code: 'flyinHigh' }, { store: 'Groupon'})
+groupon = Coupon.create(coupon_code: 'flyinHigh', store: 'Groupon')
